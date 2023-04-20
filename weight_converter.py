45e16ecb3b519fa16123e85edb2b28e217364f34
Leonard Marchant <Leno@hotmail.co.nz>
2023-04-21 11:53:57 +1200

    Added some functions to improve accuracy

--- weight_converter.py
+++ weight_converter.py
@@ -3,6 +3,6 @@
 weight = float(input("Weight: "))
 unit = str(input("(K)g or (L)bs: "))
 if unit.upper() == "K":  # previously had 'or k', but changed to use .upper
-    print("Weight in (L)bs:", (weight * 2.2))
+    print("Weight in (L)bs:", (round(float(weight * 2.2), 2)))
 elif unit.upper() == "L":
-    print("Weight in (L)bs:", (weight * 0.45))
+    print("Weight in (K)g:", (round(float(weight * 0.45), 2)))  # added float and round to add decimal number
